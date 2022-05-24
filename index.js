bbeea40ffc035248007c4ca40fefd9d20fa5ca52
luizphelip3 <luiz_phelip3@hotmail.com>
2022-05-24 17:07:45 -0300

    feat: convert json method added

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
+// aqui fazemos a importação do fs direto do npm
+const fs = require('fs')
+// utilizamos o método readFileSync que lê o arquivo setado 
+// e retorna o conteúdo dele de forma síncrona
+// e setamos um caminho cujo arquivo csv para ser convertido se encontra
 
-//aqui 
-import fs from 'fs';
+csv = fs.readFileSync("./csv/exemplo.csv", {encoding: 'utf-8'})
 
-csv = fs.readFileSync("./csv/exemplo.csv")
+// todas as linhas vão se tornar um objeto JSON e vão ser adicionadas nesse array
+let result = []
+
+// criamos um array que recebe o dado do csv até a quebra de linha 
+// (no caso define um array de  dados por linha)
+var line = csv.split("\n");
+
+// o primeiro indice do array contém todos os cabeçalhos das colunas, então a gente guarda
+// ele num array de dados da linha (indice 0 = dado de nome, indice 1 = dado de stack)
+let lineData = line[0].split(',')
+
+// separamos os cabeçalhos e vamos passar por todas as linhas do csv utilizando esse laço
+for (let i = 1; i < line.length; i++){
+    // um objeto vazio que vai guardar todos os dados convertidos e definidos
+    let obj = {}
+
+    // esse array passa pela linha atual e divide o dado quando percorrer ela
+    var currentData = line[i].split(",")
+
+    // esse laço percorre pelos headers e cria um objeto da com o conteúdo da linha,
+    // até percorrer por todos os headers
+    for(var j=0; j<lineData.length; j++){
+        obj[lineData[j]] = currentData[j]
+    }
+    result.push(obj)
+}
+
+let json = JSON.stringify(result)
+
+fs.writeFileSync('./json/output.json',json)
+
+console.log(result);
